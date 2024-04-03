3ddb7187c16fb6ea8e3c53537a55de5e7eef5451
Satyam Singh <96168882+satyam2209@users.noreply.github.com>
2024-04-03 13:18:32 +0530

    video58

---
+++ expressjs/24- video58/controllers/shop.js
@@ -0,0 +1,60 @@
+const Product = require('../models/product')
+
+
+exports.getProducts = (req, res, next) => { 
+    const callbackFnBody = (products) => {
+    // when we send template engines like pug or ejs then we render the file
+    res.render('shop/product-list', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/product-list',
+    });
+    };  
+    Product.fetchAll(callbackFnBody); // [{title: value}...]  
+}
+
+exports.getShopIndex = (req, res, next) => {
+   Product.fetchAll((products) => {
+    res.render('shop/index', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/',
+    });
+   });
+}
+
+exports.getMyCart = (req, res, next) => {
+    res.render('shop/cart', {
+        pageTitle: 'My Cart',
+        path: '/cart',
+    });
+}
+
+exports.getCheckout = (req, res, next) => {
+    res.render('shop/checkout', {
+        pageTitle: 'My Checkout',
+        path: '/checkout',
+    });
+}
+
+exports.getMyOrders = (req, res, next) => {
+    res.render('shop/orders', {
+        pageTitle: 'My Orders',
+        path: '/orders',
+    });
+}
+
+exports.getProductDetails = (req, res, next) => {
+    const productId = req.params.productId;
+    // filter the data using the productId and send that product to view
+    console.log('productId:', productId);
+    Product.findProductById(productId, (product) => {
+        console.log('Product is', product);
+        res.render('shop/product-details', {
+            pageTitle: 'My Product Details',
+            path: '/product-details',
+            product: product,
+        })
+    });
+    
+}
---
+++ expressjs/24- video58/models/product.js
@@ -0,0 +1,54 @@
+// const products = [];
+// for storing data in files
+
+const fs = require('fs');
+const path = require('path');
+
+const pathBuilt = path.join(
+    path.dirname(require.main.filename),
+    'data',
+    'products.json'
+);
+
+const getProductsFromFile = (callbackFn) => {
+    fs.readFile(pathBuilt, (err, fileContent) => {
+        if(err){
+            return callbackFn([]);
+        }
+        callbackFn(JSON.parse(fileContent));
+    });
+
+}
+
+module.exports = class Product {
+
+    constructor(_title, _description, _price, _imageUrl) {
+        this.title = _title;
+        this.description = _description;
+        this.price = _price;
+        this.imageUrl = _imageUrl;
+    }
+
+    save(){
+        this.productId = Math.round(Math.random() * 1000).toString();
+        getProductsFromFile((products) => {
+            products.push(this);
+      
+            fs.writeFile(pathBuilt, JSON.stringify(products), (err) => {
+              console.log('err', err);
+            });
+          });
+
+    }
+
+    static fetchAll(callbackFn){
+        getProductsFromFile(callbackFn);        
+    }
+
+    static findProductById(pid, callbackFn){
+        getProductsFromFile((products) => {
+           const product = products.find((product) => product.productId === pid);
+           callbackFn(product);
+        });
+    }
+};
